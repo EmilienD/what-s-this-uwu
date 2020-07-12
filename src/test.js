9b452b9761d0919c1d9c5a9d2f89538182378b3a
Durieu Emilien <contact@emilienduri.eu>
2020-07-12 16:04:10 +0200

    add puzzle, fix test

--- src/test.js
+++ src/test.js
@@ -3,15 +3,15 @@ const assert = require("assert")
 
 const puzzle = JSON.parse(fs.readFileSync(process.argv.pop()))
 
-const whats = `function(that) {
+const whats = `function(_that) {
     ${puzzle.answers
       .map(answer =>
         answer.isCorrect
-          ? `assert(that === ${answer.value}, 'answer "${answer.value}" is not equal to "this", but was expected to be');`
-          : `assert(that !== ${answer.value}, 'answer "${answer.value}" is equal to "this", but was not expected to be');`
+          ? `assert(_that === ${answer.value}, 'answer "${answer.value}" is not equal to "this", but was expected to be');`
+          : `assert(_that !== ${answer.value}, 'answer "${answer.value}" is equal to "this", but was not expected to be');`
       )
       .join("\n")}
 }`
 
-const script = puzzle.question.replace("whats", `(${whats})`)
+const script = puzzle.question.replace("whats", `;(${whats});`)
 eval(script)
