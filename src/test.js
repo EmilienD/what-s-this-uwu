ccfcee348acbb89ec750646a2cc06a96e455eac7
Durieu Emilien <contact@emilienduri.eu>
2021-07-25 17:11:58 +0200

    fix test

--- src/test.js
+++ src/test.js
@@ -1,17 +1,18 @@
 const fs = require("fs")
 const assert = require("assert")
 
-const puzzle = JSON.parse(fs.readFileSync(process.argv.pop()))
+const puzzlePath = process.argv.pop()
+const puzzle = JSON.parse(fs.readFileSync(puzzlePath))
 
 const whats = `function(_that) {
     ${puzzle.answers
       .map(answer =>
         answer.isCorrect
-          ? `assert(_that === ${answer.value}, 'answer "${answer.value}" is not equal to "this", but was expected to be');`
-          : `assert(_that !== ${answer.value}, 'answer "${answer.value}" is equal to "this", but was not expected to be');`
+          ? `assert(_that === ${answer.value}, 'answer "${answer.value}" in "${puzzlePath}" is marked correct but is not.');`
+          : `assert(_that !== ${answer.value}, 'answer "${answer.value}" in "${puzzlePath}" is correct but is not marked as such.');`
       )
       .join("\n")}
 }`
 
-const script = puzzle.question.replace("whats", `;(${whats});`)
+const script = puzzle.question.replace("whats", `;(${whats})`)
 eval(script)
